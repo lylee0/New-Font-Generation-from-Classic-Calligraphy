0beb453872e7037acc5fc52c4b1d395517da8e26
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-02-27 13:37:09 +0800

    Update image_datasets.py

--- utils/image_datasets.py
+++ utils/image_datasets.py
@@ -12,6 +12,7 @@ from . import logger
 def load_data(
     *,
     data_dir,
+    content_dir,
     batch_size,
     image_size,
     deterministic=False,
@@ -22,14 +23,25 @@ def load_data(
 ):
     if not data_dir:
         raise ValueError("unspecified data directory")
+    if not content_dir:
+        raise ValueError("unspecified content directory")
     all_files = _list_image_files_recursively(data_dir)
+    # added
+    all_contents = _list_image_files_recursively(content_dir)
+
     chars_stroke = None
-    class_names = [bf.basename(path).split("_")[0] for path in all_files]
-    sorted_classes = {x: i for i, x in enumerate(sorted(set(class_names)))}
+    class_names = [bf.basename(path).split("_")[0] for path in all_files] # 0-19 # all image names
+    sorted_classes = {x: i for i, x in enumerate(sorted(set(class_names)))} # all images name -> dict
     classes = [sorted_classes[x] for x in class_names]
-    sty_class_names = [bf.dirname(path).split("/")[-1] for path in all_files]
+    sty_class_names = [bf.dirname(path).split("/")[-1] for path in all_files] # style of each images
     sty_sorted_classes = {x: i for i, x in enumerate(sorted(set(sty_class_names)))}
     sty_classes = [sty_sorted_classes[x] for x in sty_class_names]
+
+    # added
+    content_class_names = [bf.basename(path).split("_")[0] for path in all_contents]
+    content_sorted_classes = {x: i for i, x in enumerate(sorted(set(content_class_names)))} # all images name -> dict
+    content_classes = [content_sorted_classes[x] for x in content_class_names]
+
     if stroke_path is not None:
         logger.log('=' * 20)
         logger.log("using strokes data")
@@ -48,11 +60,13 @@ def load_data(
                 assert len(char_stroke) == 32
                 chars_stroke = np.concatenate([chars_stroke, np.array(char_stroke).reshape([1, 32])], axis=0, dtype=np.float32)
 
-    if classifier_free:
+    '''if classifier_free:
         dataset = ImageDataset_Clsfree(
             image_size,
             all_files,
+            all_contents,
             classes=classes,
+            content_classes=content_classes,
             sty_classes=sty_classes,
             shard=MPI.COMM_WORLD.Get_rank(),
             num_shards=MPI.COMM_WORLD.Get_size(),
@@ -60,18 +74,20 @@ def load_data(
             random_flip=random_flip,
             chars_stroke=chars_stroke,
         )
-    else:
-        dataset = ImageDataset(
-            image_size,
-            all_files,
-            classes=classes,
-            sty_classes=sty_classes,
-            shard=MPI.COMM_WORLD.Get_rank(),
-            num_shards=MPI.COMM_WORLD.Get_size(),
-            random_crop=random_crop,
-            random_flip=random_flip,
-            chars_stroke=chars_stroke,
-        )
+    else:'''
+    dataset = ImageDataset(
+        image_size,
+        all_files,
+        all_contents,
+        classes=classes,
+        content_classes=content_classes,
+        sty_classes=sty_classes,
+        shard=MPI.COMM_WORLD.Get_rank(),
+        num_shards=MPI.COMM_WORLD.Get_size(),
+        random_crop=random_crop,
+        random_flip=random_flip,
+        chars_stroke=chars_stroke,
+    )
     if deterministic:
         loader = DataLoader(
             dataset, batch_size=batch_size, shuffle=False, num_workers=1, drop_last=True
@@ -101,7 +117,9 @@ class ImageDataset(Dataset):
         self,
         resolution,
         image_paths,
+        content_paths,
         classes=None,
+        content_classes=None,
         sty_classes=None,
         shard=0,
         num_shards=1,
@@ -111,9 +129,13 @@ class ImageDataset(Dataset):
     ):
         super().__init__()
         self.resolution = resolution
-        self.local_images = image_paths[shard:][::num_shards]
-        self.local_classes = None if classes is None else classes[shard:][::num_shards]
+        self.local_images = image_paths[shard:][::num_shards] # path of all images
+        self.local_contents = content_paths[shard:][::num_shards] # added
+
+        self.local_classes = None if classes is None else classes[shard:][::num_shards] # 0 to 2999, repeat 20
+        self.content_classes = None if content_classes is None else content_classes[shard:][::num_shards] # added
         self.style_classes = None if sty_classes is None else sty_classes[shard:][::num_shards]
+
         self.random_crop = random_crop
         self.random_flip = random_flip
         self.chars_stroke = chars_stroke
@@ -123,6 +145,13 @@ class ImageDataset(Dataset):
             self.sty_classes_idxs = {}
             for sty_class in sty_classes_set:
                 self.sty_classes_idxs[sty_class] = np.where(np.array(self.style_classes) == sty_class)[0]
+
+        # added
+        if self.content_classes is not None:
+            con_classes_set = set(self.content_classes)
+            self.con_classes_idxs = {}
+            for con_class in con_classes_set:
+                self.con_classes_idxs[con_class] = np.where(np.array(self.content_classes) == con_class)[0]
 
     def __len__(self):
         return len(self.local_images)
@@ -146,11 +175,14 @@ class ImageDataset(Dataset):
 
         out_dict = {}
         if self.local_classes is not None:
-            out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
+            # out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
             out_dict["mask_y"] = False
+
+        # style
         if self.style_classes is not None:
-            sty_img_idxs = self.sty_classes_idxs[self.style_classes[idx]]
-            rand_sty_idx = np.random.choice(sty_img_idxs)
+            # len(self.sty_classes_idxs) = 19
+            sty_img_idxs = self.sty_classes_idxs[self.style_classes[idx]] #len(self.style_classes) = 56916
+            rand_sty_idx = np.random.choice(sty_img_idxs) # sty_img_idxs is an array
             sty_img_path = self.local_images[rand_sty_idx]
             with bf.BlobFile(sty_img_path, "rb") as f:
                 sty_pil_image = Image.open(f)
@@ -158,19 +190,33 @@ class ImageDataset(Dataset):
             sty_pil_image = sty_pil_image.convert("RGB")
             sty_arr = center_crop_arr(sty_pil_image, self.resolution)
             sty_arr = sty_arr.astype(np.float32) / 127.5 - 1
+
+            # content
+            if self.local_contents is not None:
+                con_img_path = self.local_contents[self.local_classes[idx]]
+                with bf.BlobFile(con_img_path, "rb") as f:
+                    con_pil_image = Image.open(f)
+                    con_pil_image.load()
+                con_pil_image = con_pil_image.convert("RGB")
+                con_arr = center_crop_arr(con_pil_image, self.resolution)
+                con_arr = con_arr.astype(np.float32) / 127.5 - 1            
+
+            # stroke
             if self.chars_stroke is not None:
                 out_dict["stroke"] = self.chars_stroke[self.local_classes[idx]]
                 out_dict["mask_stroke"] = False
-            return [np.transpose(arr, [2, 0, 1]), np.transpose(sty_arr, [2, 0, 1])], out_dict  # [batch, sty_batch, condition] ******
+            return [np.transpose(arr, [2, 0, 1]), np.transpose(sty_arr, [2, 0, 1]), np.transpose(con_arr, [2, 0, 1])], out_dict  #change this
 
         return np.transpose(arr, [2, 0, 1]), out_dict
 
-class ImageDataset_Clsfree(Dataset):
+'''class ImageDataset_Clsfree(Dataset):
     def __init__(
         self,
         resolution,
         image_paths,
+        content_paths,
         classes=None,
+        content_classes=None,
         sty_classes=None,
         shard=0,
         num_shards=1,
@@ -181,6 +227,7 @@ class ImageDataset_Clsfree(Dataset):
         super().__init__()
         self.resolution = resolution
         self.local_images = image_paths[shard:][::num_shards]
+        self.local_contents = content_paths[shard:][::num_shards] # added
         self.local_classes = None if classes is None else classes[shard:][::num_shards]
         self.style_classes = None if sty_classes is None else sty_classes[shard:][::num_shards]
         self.random_crop = random_crop
@@ -235,7 +282,7 @@ class ImageDataset_Clsfree(Dataset):
                 out_dict["stroke"] = self.chars_stroke[self.local_classes[idx]]
                 out_dict["mask_stroke"] = random.random() < 0.3
             return [np.transpose(arr, [2, 0, 1]), np.transpose(sty_arr, [2, 0, 1])], out_dict
-        return np.transpose(arr, [2, 0, 1]), out_dict
+        return np.transpose(arr, [2, 0, 1]), out_dict'''
 
 def center_crop_arr(pil_image, image_size):
     # We are not on a new enough PIL to support the `reducing_gap`
